5654dadd305bb49ae947bb0015b6ff25617d822f
Yun Jung Hyeon <yjh54240@naver.com>
2017-08-23 22:58:03 +0900

    회원가입 수정 xml 추가

--- Sing-for-you/app/src/main/java/android/sinabro/sing_for_you/activities/SignUp.java
+++ Sing-for-you/app/src/main/java/android/sinabro/sing_for_you/activities/SignUp.java
@@ -1,14 +1,30 @@
 package android.sinabro.sing_for_you.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.sinabro.sing_for_you.R;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SignUp extends AppCompatActivity {
+
+    private ImageView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        backButton=(ImageView)findViewById(R.id.ib_toolbar_back);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
